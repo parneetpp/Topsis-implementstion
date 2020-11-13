1bc8bcce6754ea3c5b7190a664e3a6e9f4faed4a
Parneet singh <parneetpp@gmail.com>
2020-11-13 20:37:51 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
 setup(
     name="TOPSIS-PARNEET-101803281",
     packages=["TOPSIS-PARNEET-101803281"],
